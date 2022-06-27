0121a9f5de6848187648723b1e94bc395e0ac2bd
leonardo-juliano <99771244+leonardo-juliano@users.noreply.github.com>
2022-06-26 22:47:53 -0300

    atualizado

--- main.py
+++ main.py
@@ -296,10 +296,10 @@ def visualizar_pagamentos():
   
 
 #--------------------------------------------------------
-if __name__ == '__main__':
+if __name__ == 'main':
     port = int(os.getenv('PORT'), '5000')
-    app.run(host='0.0.0.0', port = port)
-    app.run(debug=True)  
+    app.run(debug=True,host='0.0.0.0', port = port)
+     
 
     
     
